74e6191acd39dad8fb1927a117b4842f34622071
Jessica Bathel <46831166+jbathel@users.noreply.github.com>
2022-06-22 09:21:54 -0700

    Added update functionality to Word

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,7 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = require('graphql');
 
 // Word Type
@@ -42,6 +43,64 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    // Add word
+    addWord: {
+      type: WordType,
+      args: {
+        word: { type: GraphQLNonNull(GraphQLString) },
+        wordLength: { type: GraphQLNonNull(GraphQLString) },
+        wordGroup: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const word = new Word({
+          word: args.word,
+          wordLength: args.wordLength,
+          wordGroup: args.wordGroup,
+        });
+        return word.save();
+      },
+    },
+    // Delete word
+    deleteWord: {
+      type: WordType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Word.findByIdAndRemove(args.id);
+      },
+    },
+    //  Update a word
+    updateWord: {
+      type: WordType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        word: { type: GraphQLString },
+        wordLength: { type: GraphQLString },
+        wordGroup: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return Word.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              word: args.word,
+              wordLength: args.wordLength,
+              wordGroup: args.wordGroup,
+            },
+          },
+          { new: true }
+        );
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
